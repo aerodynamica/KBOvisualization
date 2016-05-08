083efa1f968501157c5a2318f434312695b6e748
t-vince <vincent.tanghe@gmail.com>
2016-05-08 18:22:11 +0200

    Added bars per juridical form

--- infofish.horizontal-bar.js
+++ infofish.horizontal-bar.js
@@ -1,113 +1,87 @@
 function juridicalForms() {
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 0};
-    var width = window.innerWidth/4 - margin.left - margin.right;
-    var barHeight = 20;
+    var margin = {top: 20, right: 10, bottom: 30, left: 10};
+    var width = $("#barchart").width();
+    var barHeight = 15;
+    var nbForms = 5;
 
-    var nbForms = 12;
-
-    
-    
 
     function draw(data, color) {
-         //var start = new Date().getTime();
+        color = "#EEEEEE";
+         var start = new Date().getTime();
         
+        // Dynamically change the title
+        if(data.length < 1) 
+            $("#barchart").prev().find("span").html("Onbekende berijfsvorm");
+        else if(data.length < 2) 
+            $("#barchart").prev().find("span").html("Enige bedrijfsvorm");
+        else if(data.length < 5) 
+            $("#barchart").prev().find("span").html("Top "+data.length+" bedrijfsvormen");
+
+        // Sort according to count & cut to max top list
         data.sort(function(a, b) { return b.count - a.count; });
-        //data.sort(function(a,b) {return (a.count < b.count) ? 1 : 0;} );
         var data = data.slice(0, nbForms);
 
-        var barchart = d3.select("#barchart svg");
-                
-        if(barchart.empty()){
-            d3.select("#barchart").append("svg")
-                            .attr("width", width)
-                            .attr("height", barHeight * nbForms);
-        }
-        
-         var tip = d3.tip()
-         .attr('class', 'd3-tip')
-         .html(function(d) { return "Aantal ondernemingen: "+d.count.toLocaleString()+"</br>Rechtsvorm: "+d.form;})
-         .direction('e')
-         .offset([0,10]);
+        // Start building barchart
+        $("#barchart").html('');
+        var barchart = d3.select("#barchart")
+                         .append("svg")
+                         .attr("width", width)
+                         .attr("height", barHeight * nbForms);;
 
+        // Hover tip message    
+        var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .html(function(d) { return d.form + "<br/>Ondernemingen: "+d.count.toLocaleString(); })
+            .direction('e')
+            .offset([0,10])
+            .direction('e');
+          
+        barchart.call(tip);
         
-                           
-
         var scale = d3.scale.linear()
                 .domain([0, d3.max(data, function(d) { return d.count; })] )
                 .range([0,500]);
         
-        
-
-          var bar = barchart.selectAll("g")
+        var bar = barchart.selectAll("g")
              .data(data, function(d) {return d.form;});
-            /* .transition().duration(1000)*/
-             
-          
-          barchart.call(tip);
-          
-          bar.transition().duration(1000)
-             .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });   
-     
-          bar.select("rect")
-             /*.transition().duration(1000)*/
-             .attr("width", function(d) { return scale(d.count); })
-             /*.attr("height", barHeight - 1)
-             .attr("class", "bar")*/
-             .style("fill", color);
-            /* .append("svg:title")
-             .text(function(d) { return "Vestigingseenheden: "+d.count+"\nBedrijfsvorm: "+d.form; });*/
-     
-          bar.select("text")
-             /*.attr("x", 0)
-             .attr("y", barHeight / 2)
-             .attr("dy", ".35em")
-             .attr("transform", "translate(" + 5 + ", 0)")*/
-             .text(function(d) {
-                 var maxlength = width/7
-                 if(d.form.length > maxlength)
-                    return d.form.slice(0, maxlength)+"... ("+d.count+")";
-                  return d.form+" ("+d.count+")"; });
-           /*  .append("svg:title")
-             .text(function(d) { return "Vestigingseenheden: "+d.count+"\nBedrijfsvorm: "+d.form; });*/
-     
+
    
-        var enter = bar.enter().append("g");
-                       
-        enter/*.transition().duration(1000)*/
-                        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
-                .on("mouseover", tip.show)
-              .on("mouseout", tip.hide);
-                        
-        enter.append("rect")   
-            /* .transition().duration(1000)*/
+        var enter = bar.enter().append("g");    
+        enter.attr("transform", function(d, i) { return "translate(5," + i * barHeight + ")"; });            
+        enter.append("rect")
              .attr("width", function(d) { return scale(d.count); })
-            .attr("height", barHeight - 1)
+             .attr("height", barHeight - 1)
              .attr("class", "bar")
              .style("fill", color);
-     
+             /*.on("mouseover", tip.show)
+             .on("mouseout", tip.hide);*/
+        
          enter.append("text")
              .attr("x",0)
              .attr("y", barHeight / 2)
              .attr("dy", ".35em")
-             .attr("transform", "translate(" + 5 + ", 0)")
-             .text(function(d) {
-                 var maxlength = width/7
-                 if(d.form.length > maxlength)
-                    return d.form.slice(0, maxlength)+"... ("+d.count.toLocaleString()+")";
-                  return d.form+" ("+d.count.toLocaleString()+")"; });
+             .attr("transform", "translate(" + 0 + ", 0)")
+             .text(function(d, i) {
+                 var afkorting = d.form
+                                  .replace("met", "")
+                                  .replace("onder de vorm van", "q")
+                                  .replace("onder vorm van", "q")
+                                  .replace("RSZ", "x")
+                                  .replace(" van", "")
+                                  .replace(" of", "")
+                                  .match(/\b(\w)/g);
+                    if(afkorting.length > 1)
+                        afkorting = afkorting.join('').replace("q", " ").replace("x", " RSZ ").toUpperCase();
+                    else 
+                        afkorting = afkorting[0]
+                 var index = (i+1).toLocaleString()+". ";
+                 return index+afkorting;
+              })
+             .on("mouseover", tip.show)
+             .on("mouseout", tip.hide);
         
-        
-        
-     
-     
         bar.exit().remove();
-        
-        //var stop = new Date().getTime();
-        
-        //console.log("drew bar chart in "+(stop-start)+" ms");
-        
-       
      
     };
 
